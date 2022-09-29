f1b53bbd6140ee149b036cd1ab212005a9dd04cf
IgnacioIA <ignaciocruz1@hotmail.com>
2022-09-29 11:07:23 -0300

    ingreso mi primer commit

---
+++ Persona.h
@@ -0,0 +1,17 @@
+#ifndef PERSONA_H_INCLUDED
+#define PERSONA_H_INCLUDED
+
+#include "Lista.h"
+struct PersonaEstructura;
+
+typedef struct PersonaEstructura * Persona;
+
+Persona crearPersona(char  n[20], float p);
+
+void mostrarPersona(Persona p);
+
+void mostrarListaPersonas (Lista lista);
+
+float getPeso(Persona p);
+
+#endif // PERSONA_H_INCLUDED
